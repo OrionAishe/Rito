73744db53ab6149ee99e84d91bd764ca0d3c5fb3
OrionCigano <wagantonucci@hotmail.com>
2021-02-02 18:44:17 -0300

    create landing page and start js

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var
+height = 0;
+result = 0;
+
+function Conversor(height){
+height = document.getElementById("height");
+result = height/1.54;
+return result;
+//document.getElementById("result").innerHTML = "Você tem", result ,"Rito Gomes de altura";
+}
