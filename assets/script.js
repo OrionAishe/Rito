72b69d0bf46c4e1a3a7060b55f1d43c652edc473
OrionCigano <wagantonucci@hotmail.com>
2021-02-02 20:08:53 -0300

    finish js

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
-var
-height = 0;
-result = 0;
+const button = document.querySelector('.submit');
+const altura = document.querySelector('.altura');
+const resultado = document.querySelector('.resultado')
 
-document.getElementById("submit").addEventListener("click", Conversor);
-
-function Conversor(){
-height = document.getElementById("height");
-result = height/1.54;
-alert(result);
+button.onclick= () => {
+let result = altura.value/1.54;
+result = Math.round(result * 100) / 100
+if(result > 0){
+    resultado.textContent = result;
+}
+else{
+    resultado.textContent = `${altura.value} não é um Número`;
+}
 }
