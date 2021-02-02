a02777fc4abf23829a2acfe66873698344e12d3b
OrionCigano <wagantonucci@hotmail.com>
2021-02-02 20:31:06 -0300

    fix minor issues in js and html

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
 const button = document.querySelector('.submit');
 const altura = document.querySelector('.altura');
-const resultado = document.querySelector('.resultado')
+const resultado = document.querySelector('.resultado');
 
 button.onclick= () => {
-let result = altura.value/1.54;
-result = Math.round(result * 100) / 100
-if(result > 0){
-    resultado.textContent = `Você tem ${result} Ritos de altura!;
-}
-else{
-    resultado.textContent = `${altura.value} não é um Número`;
-}
+    let result = Math.round((parseFloat(altura.value.replace(",", "."))/154) * 100) / 100;
+    if(result > 0){
+        resultado.textContent = `Você tem ${result} Rito Gomes de altura!`;
+    }
+    else{
+        resultado.textContent = `${altura.value} não é um Número`;
+    }
 }
